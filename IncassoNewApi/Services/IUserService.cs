63415461bd9f478575e7d8371237c458feac5b74
Kausar Mehmood <khan.kausar@gmail.com>
2019-12-03 20:55:13 +0500

    angular ui

---
+++ IncassoNewApi/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using IncassoNewApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncassoNewApi.Services
+{
+    interface IUserService
+    {
+        Inc_User Authenticate(string username, string password);
+        IEnumerable<Inc_User> GetAll();
+    }
+}
